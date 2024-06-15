0a795f9228d9405770671fe3e5d5314f360bc806
LeoFhin <leonardo.santos7507@gmail.com>
2024-06-15 16:29:48 -0300

    booleanos

---
+++ Java/src/Operadores_Booleanos/Main.java
@@ -0,0 +1,18 @@
+package Operadores_Booleanos;
+
+public class Main {
+    public static void main(String[] args){
+
+        boolean fimDeSemana = true;
+        boolean fazendoSol = true;
+        boolean vamosAPraia = fimDeSemana && fazendoSol;
+        System.out.println(vamosAPraia);
+
+        //Tabela verdade
+        //Operador && (AND)
+        //true && true = true
+        //true && false = false
+        //false && true = false
+        //false && true = false
+    }
+}
