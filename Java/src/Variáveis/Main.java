30d3d1b0ac9869eefb7167336437e59c4f8c082d
LeoFhin <leonardo.santos7507@gmail.com>
2024-06-15 16:20:43 -0300

    Variáveis em java

---
+++ Java/src/Variáveis/Main.java
@@ -0,0 +1,19 @@
+package Variáveis;
+
+public class Main  {
+    public static void main(String[] args){
+
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        int divisao = a/b;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+    }
+}
