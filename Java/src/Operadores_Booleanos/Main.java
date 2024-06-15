d1c2aa37ea2a16c9e966e6dd119bdd48912e55fd
LeoFhin <leonardo.santos7507@gmail.com>
2024-06-15 16:39:02 -0300

    tabela verdade

--- Java/src/Operadores_Booleanos/Main.java
+++ Java/src/Operadores_Booleanos/Main.java
@@ -8,11 +8,21 @@ public class Main {
         boolean vamosAPraia = fimDeSemana && fazendoSol;
         System.out.println(vamosAPraia);
 
-        //Tabela verdade
-        //Operador && (AND)
-        //true && true = true
-        //true && false = false
-        //false && true = false
-        //false && true = false
+        // Tabela verdade
+
+        // Operador && (AND)
+        // true && true = true
+        // true && false = false
+        // false && true = false
+        // false && true = false
+
+        // Operado || (OR)
+        // true || true = true
+        // true || false = true
+        // false || false = true
+        // false || false = false
+
+        String mensagem = fimDeSemana ? "É fim de semana" : "Não é fim de semana";
+        System.out.println(mensagem);
     }
 }
